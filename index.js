4215505d32bda6118eb2238c22c7521cd245626f
desh01prajakta <prajudeshmukh11@gmail.com>
2023-11-15 20:45:33 -0600

    adds functionality

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const userInputString = window.prompt("Enter Froyo Flavors", "coffee, vanilla, strawberry, chocolate");
+
+
+const userInputString = window.prompt("Enter Froyo Flavors", "coffee,vanilla,strawberry");
 
 const stringArray = userInputString.split(",");
 
-const numbers = [];
-for(let i = 0; i< stringArray.length; i++){
-    const str = stringArray[i];
-    const number = parseInt(str);
-    numbers.push(number);
+console.log(stringArray);
+
+function counting(stringArray){
+let flavors = {
+    vanilla: 0,
+    coffee: 0,
+    strawberry: 0
+    }
+for(let i = 0; i < stringArray.length; i++){
+    if(stringArray[i] == "vanilla"){
+        flavors["vanilla"]+=1;
+    }else if(stringArray[i] == "strawberry"){
+        flavors["strawberry"]+=1;
+    }else if(stringArray[i] == "coffee") {
+        flavors["coffee"]+=1;
+    }
+} return flavors;
 }
-console.log(numbers);
+console.log(counting(stringArray));
 
 
-function getFlavors(flavors){
-    const totalFlavors = 0;
-    for(let i = 0; i <flavors.length; i++){
-    totalFlavors = flavors[i]};
-    return totalFlavors;
-
-}
-console.log(totalFlavors(numbers));
-
-function getLength(numbers){
-    return numbers.length
-}
 
